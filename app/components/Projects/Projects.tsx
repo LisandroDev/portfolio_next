018186a1bbaf4efd4828c21a0fbf6bc4e28ee297
lichaa824 <lisandrorubi@gmail.com>
2023-06-13 18:40:27 -0300

    Add URl Shortener project

--- app/components/Projects/Projects.tsx
+++ app/components/Projects/Projects.tsx
@@ -2,10 +2,13 @@ import { ProjectCard } from "./ProjectCard/ProjectCard";
 import neithaImg from "../../assets/test.png";
 import budgetAppImg from "../../assets/projectsimages/budgetapp.png";
 import whereisAppImg from "../../assets/projectsimages/whereis.jpeg"
+import urlShortenerImg from '@/app/assets/UrlShortener.png'
 
 const neithaclothesDescription = `Neithaclothes is an online platform for buying products. It provides
 a user-friendly interface for browsing, purchasing, and shipping
 items.`;
+
+const URLShortenerDescription = `Simplify and optimize your links with our powerful tool. Transform long URLs into concise, memorable ones for easy sharing and improved online experience. Track link performance .`;
 
 const budgetAppDescription = `Budget app was created as part for Alkemy Challenge,that required participants to build a budget app that could separate income and expenses, using a SQL database.`;
 
@@ -14,6 +17,14 @@ const whereisAppDescription = `"Where's Waldo" style game, players must find thr
 export const Projects = () => {
   return (
     <div className="container-md m-5 gap-5 flex flex-wrap">
+        <ProjectCard
+        image={urlShortenerImg}
+        title="URL Shortener"
+        stack={["React", "PostgreSQL", "Next.JS","TailwindCSS", "DaisyUI", "Prisma"]}
+        description={URLShortenerDescription}
+        code="https://github.com/LisandroDev/url_shortener"
+        liveDemo="https://urlshort-psi.vercel.app/"
+      />
       <ProjectCard
         image={neithaImg}
         title="Neithaclothes"

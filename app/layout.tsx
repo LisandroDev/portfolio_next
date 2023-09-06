9c318464742492f78f54d1c1efe71a05cafb2341
lichaa824 <lisandrorubi@gmail.com>
2023-09-06 17:00:44 -0300

    Remove arrow for navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import "./globals.css";
 import { ScrollTo } from "./utils/ScrollTo";
 import { Poppins } from "next/font/google";
-const font = Poppins({
-    subsets: ["latin"],
-    weight: ["500", "600"],
-  });
+import { Roboto } from "next/font/google"
+const font = Roboto({
+  subsets: ["latin"],
+  weight: ["500", "700"],
+});
 export const metadata = {
   title: "Lisandro Rubianes",
   description: "Portfolio",
@@ -18,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>{children}
-      <div className="flex justify-center fixed bottom-0 inset-x-0 mb-12"><ScrollTo id="content" label="Go to content" /></div> 
+
       </body>
-      
+
     </html>
   );
 }
